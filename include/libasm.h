b349fd043002fc87b4be86e57e2747b8f948ff8a
Daniel Poveda salgado <dpoveda-@c1r17s2.42madrid.com>
2023-05-08 10:48:08 +0200

    feat: add lib header file

---
+++ include/libasm.h
@@ -0,0 +1,14 @@
+#ifndef LIBASM_LIBASM_H_
+#define LIBASM_LIBASM_H_
+
+#include <stddef.h>
+#include <sys/types.h>
+
+size_t ft_strlen(const char* s);
+char* ft_strcpy(char* dest, const char* src);
+int ft_strcmp(const char* s1, const char* s2);
+ssize_t ft_write(int fd, const void* buf, size_t count);
+ssize_t ft_read(int fd, void* buf, size_t count);
+char* ft_strdup(const char* s);
+
+#endif // LIBASM_LIBASM_H_
