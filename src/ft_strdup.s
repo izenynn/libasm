9a29826ba1096b41b1bbbc3b28fd26736bc0c61d
izenynn <me@izenynn.com>
2023-05-09 16:10:16 +0200

    feat: strdup

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -1,5 +1,60 @@
-section .text
-    global _ft_strdup
+; function: _ft_strdup
+; char* ft_strdup(const char* s);
+;
+; input:
+; rdi - address of the string to duplicate
+;
+; output:
+; rax - returns a pointer to the copy of s,
+;       if insufficient memory returns NULL and errno is set to ENOMEM
+;
+; clobbered registers:
+; rax, rbx
 
+    global _ft_strdup
+    extern _ft_strlen
+    extern _ft_strcpy
+    extern _malloc
+    extern ___error
+
+    section .text
 _ft_strdup:
+    ; save callee-saved registers (and align the stack in this case)
+    push rbx
+
+    ; get string len + 1 (for '\0')
+    call _ft_strlen
+    inc rax
+
+    ; save string pointer
+    mov rbx, rdi
+
+    ; set first argument for malloc
+    mov rdi, rax
+    ; allocate memory
+    call _malloc
+    jc .error
+
+    ; set first argument to allocated pointer
+    mov rdi, rax
+    ; set second argument to string pointer
+    mov rsi, rbx
+    ; copy string to allocated pointer
+    call _ft_strcpy
+
+    jmp .done
+.error:
+    ; save errno
+    mov ebx, eax
+    ; get errno address
+    call ___error
+    ; put errno in return value of ___error (pointer to errno)
+    mov dword [rax], ebx
+
+    ; error returns -1
+    mov rax, -1
+.done
+    ; restore callee-saved registers (and fix up the stack in this case)
+    pop rbx
+
     ret
