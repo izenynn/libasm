faf37755ff32cb7a3c1a3caba6dd02805abd49f9
izenynn <me@izenynn.com>
2023-05-09 12:06:18 +0200

    feat: strcmp

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -1,5 +1,37 @@
+; function: _ft_strcmp
+; int ft_strcmp(const char* s1, const char* s2);
+;
+; input:
+; rdi - address of s1 string
+; rsi - address of s2 string
+;
+; output:
+; rax - address of the dest string
+; rax - integer difference according as s1 is >,== or < than s2
+;
+; clobbered registers:
+; rax, rcx, rdx
+
 section .text
     global _ft_strcmp
 
 _ft_strcmp:
+    mov rax, -1
+.loop:
+    inc rax
+
+    mov cl, byte [rdi + rax]
+    mov dl, byte [rsi + rax]
+
+    cmp cl, dl
+    jne .done
+
+    test cl, cl
+    jz .done
+
+    jmp .loop
+.done
+    movzx rax, cl
+    movzx rdx, dl
+    sub rax, rdx
     ret
