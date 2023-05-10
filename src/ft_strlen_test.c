2b7e98adae9571967d4d8ea550df8e61fdc0fe49
izenynn <me@izenynn.com>
2023-05-10 21:20:19 +0200

    feat: add strlen tests

---
+++ src/ft_strlen_test.c
@@ -0,0 +1,25 @@
+#include "miniunit.h"
+
+#include <signal.h>
+
+#include "libasm.h"
+
+TEST_CASE(normal) {
+  char s[] = "hello world!";
+  ASSERT(ft_strlen(s) == 12, "wrong length returned for \"hello world!\"");
+}
+TEST_CASE(one_char) {
+  char s[] = "a";
+  ASSERT(ft_strlen(s) == 1, "wrong length returned for \"a\"");
+}
+TEST_CASE(empty_string) {
+  char s[] = "";
+  ASSERT(ft_strlen(s) == 0, "wrong length returned for \"\"");
+}
+
+TEST_MAIN {
+  RUN_TEST(normal);
+  RUN_TEST(one_char);
+  RUN_TEST(empty_string);
+  END();
+}
